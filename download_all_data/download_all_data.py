a47166f5db2e4a77c7e9f5cf96ee5a58705fae5c
Benedek4000 <kovacsbenedek4000@gmail.com>
2023-03-07 17:48:48 +0000

    automatically removed warnings for download_all_data.py

--- download_all_data/download_all_data.py
+++ download_all_data/download_all_data.py
@@ -1,9 +1,10 @@
 import fastf1
-from fastf1 import plotting
 import datetime
+import warnings
+
+warnings.filterwarnings("ignore")
 
 fastf1.Cache.enable_cache('D:/f1data')
-plotting.setup_mpl()
 
 for year in range(2018,datetime.date.today().year+1,1):
     schedule=fastf1.get_event_schedule(year, include_testing=False)
