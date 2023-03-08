bfaafee61766b48801f6f5b67405a4181816c10d
Benedek4000 <kovacsbenedek4000@gmail.com>
2023-03-08 18:20:21 +0000

    added multithreading to download_all_data.py

--- download_all_data/download_all_data.py
+++ download_all_data/download_all_data.py
@@ -1,13 +1,23 @@
+from multiprocessing.pool import ThreadPool as Pool
 import fastf1
 import datetime
 import warnings
 
 warnings.filterwarnings("ignore")
-
 fastf1.Cache.enable_cache('D:/f1data')
+
+pool_size = 10
+pool = Pool(pool_size)
+
+def loadGP(year, gp):
+    for session in range(1,6,1):
+        fastf1.get_session(year, gp, session).load()
+    return None
 
 for year in range(2018,datetime.date.today().year+1,1):
     schedule=fastf1.get_event_schedule(year, include_testing=False)
     for gp in schedule['Country']:
-        for session in range(1,6,1):
-            fastf1.get_session(year, gp, session).load()
+        pool.apply_async(loadGP, (year, gp,))
+
+pool.close()
+pool.join()
